e7d8d459d9d88be1997cc33c90f36072f5b7f06b
athenayun <shungieachianhuhh@gmail.com>
2018-04-24 10:48:30 +0800

    modified q4.sql

--- mysql/q4.sql
+++ mysql/q4.sql
@@ -11,3 +11,25 @@ group by CarBrand_Ultimate ;
 select *
 FROM db_test.group4data_trial_5
 where TotalKM2017 = '-' ;
+
+describe brand_mazda;
+
+
+SELECT * FROM db_test.group4data_trial_complete_wthym;
+
+select (2018- CarProduced_Year )
+FROM db_test.group4data_trial_complete_wthym ;
+
+select count(*) FROM db_test.group4data_trial_complete_wthym ;
+
+select NightPassingRatio, count(*)
+FROM db_test.group4data_trial_complete_wthym
+group by NightPassingRatio ;
+
+select RegisteredCarCategory, round(avg(DayPassingRatio), 2), round(avg(NightPassingRatio), 2)
+FROM db_test.group4data_trial_complete_wthym
+group by RegisteredCarCategory ;
+
+select CarProduced_Month, count(*)
+FROM db_test.group4data_trial_complete_wthym
+group by CarProduced_Month ;
